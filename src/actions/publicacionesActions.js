c51d412d6f512e4f879f2da1d57aab94ed313fe6
Sergio Ruiz <shecho27@gmail.com>
2020-09-02 22:02:59 -0500

    Avance del modulo de compartir información - Revisión de errores

--- src/actions/publicacionesActions.js
+++ src/actions/publicacionesActions.js
@@ -1,6 +1,10 @@
-import axios from 'axios'
-import { TRAER_POR_USUARIO, CARGANDO, ERROR } from "../types/publicacionesTypes";
-import * as usuariosTypes from './types/usuariosTypes'
+import axios from "axios";
+import {
+  TRAER_POR_USUARIO,
+  CARGANDO,
+  ERROR,
+} from "../types/publicacionesTypes";
+import * as usuariosTypes from "../types/usuariosTypes";
 
 // export const traerTodos = () => async (dispatch) => {
 //     dispatch({
@@ -21,33 +25,32 @@ import * as usuariosTypes from './types/usuariosTypes'
 //     }
 // }
 
-const {TRAER_TODOS: USUARIOS_TRAER_TODOS} = usuariosTypes;
+const { TRAER_TODOS: USUARIOS_TRAER_TODOS } = usuariosTypes;
 
 export const traerPorUsuario = (key) => async (dispatch, getState) => {
-    const {usuarios} = getState().usuariosReducer
-    const {publicaciones} = getState().publicacionesReducer
-    const usuario_id = usuarios[key].id
-    const respuesta = await axios.get(`https://jsonplaceholder.typicode.com/posts?userId=${usuario_id}`)
+  const { usuarios } = getState().usuariosReducer;
+  const { publicaciones } = getState().publicacionesReducer;
+  const usuario_id = usuarios[key].id;
+  const respuesta = await axios.get(
+    `https://jsonplaceholder.typicode.com/posts?userId=${usuario_id}`
+  );
 
-    const publicaciones_actualizadas = [
-        ...publicaciones,
-        respuesta.data
-    ];
+  const publicaciones_actualizadas = [...publicaciones, respuesta.data];
 
-    const publicaciones_key = publicaciones_actualizadas.length - 1;
-    const usuarios_actualizados = [...usuarios];
-    usuarios_actualizados[key] = {
-        ...usuarios[key],
-        publicaciones_key
-    }
+  const publicaciones_key = publicaciones_actualizadas.length - 1;
+  const usuarios_actualizados = [...usuarios];
+  usuarios_actualizados[key] = {
+    ...usuarios[key],
+    publicaciones_key,
+  };
 
-    dispatch({
-        type: USUARIOS_TRAER_TODOS,
-        payload: usuarios_actualizados
-    })
+  dispatch({
+    type: USUARIOS_TRAER_TODOS,
+    payload: usuarios_actualizados,
+  });
 
-    dispatch({
-        type: TRAER_POR_USUARIO,
-        payload: publicaciones_actualizadas
-    })
-}
+  dispatch({
+    type: TRAER_POR_USUARIO,
+    payload: publicaciones_actualizadas,
+  });
+};
--- src/components/Publicaciones/index.js
+++ src/components/Publicaciones/index.js
@@ -1,55 +1,110 @@
-import React, { Component } from 'react'
-import {connect} from 'react-redux';
-import Spinner from '../general/Spinner'
-import Fatal from '../general/Fatal'
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import Spinner from "../../general/Spinner";
+import Fatal from "../../general/Fatal";
 import * as usuariosActions from "../../actions/usuariosActions";
 import * as publicacionesActions from "../../actions/publicacionesActions";
 
-const {traerTodos: usuariosTraerTodos} = usuariosActions
-const {traerPorUsuario: publicacionesTraerPorUsuario} = publicacionesActions
+const { traerTodos: usuariosTraerTodos } = usuariosActions;
+const { traerPorUsuario: publicacionesTraerPorUsuario } = publicacionesActions;
 
 class Publicaciones extends Component {
-    async componentDidMount(){
-        const {
-            usuariosTraerTodos,
-            publicacionesTraerPorUsuario,
-            match: { params: {key} }
-        } = this.props;
+  async componentDidMount() {
+    const {
+      usuariosTraerTodos,
+      publicacionesTraerPorUsuario,
+      match: {
+        params: { key },
+      },
+    } = this.props;
 
-        if(!this.props.usuariosReducer.usuarios.length){
-            await usuariosTraerTodos()
-        }
-        if(!('publicaciones_key' in this.props.usuariosReducer.usuarios[key])){
-            publicacionesTraerPorUsuario(key)
-        }
+    if (!this.props.usuariosReducer.usuarios.length) {
+      await usuariosTraerTodos();
     }
+    if (this.props.usuariosReducer.error) {
+      return;
+    }
+    if (!("publicaciones_key" in this.props.usuariosReducer.usuarios[key])) {
+      publicacionesTraerPorUsuario(key);
+    }
+  }
 
-    ponerUsuario = () => {
-        const {
-            usuariosReducer,
-            match: { params: {key} }
-        }
+  ponerUsuario = () => {
+    const {
+      usuariosReducer,
+      match: {
+        params: { key },
+      },
+    } = this.props;
+
+    if (!usuariosReducer.usuarios.length || usuariosReducer.cargando) {
+      return <Spinner />;
     }
-    render() {
-        return (
-            <div>
-                <h1>Publicaciones de </h1>
-                {this.props.match.params.key}
-            </div>
-        )
+    if (usuariosReducer.error) {
+      return <Fatal mensaje={usuariosReducer.error} />;
     }
+    const nombre = usuariosReducer.usuarios[key].name;
+    return <h1>Publicaciones de {nombre}</h1>;
+  };
+
+  ponerPublicaciones = () => {
+    const {
+      usuariosReducer,
+      usuariosReducer: { usuarios },
+      publicacionesReducer,
+      publicacionesReducer: { publicaciones },
+      match: {
+        params: { key },
+      },
+    } = this.props;
+
+    if (!usuarios.length) {
+      return;
+    }
+    if (usuariosReducer.error) {
+      return;
+    }
+    if (publicacionesReducer.cargando) {
+      return <Spinner />;
+    }
+    if (publicacionesReducer.error) {
+      return <Fatal mensaje={publicacionesReducer.error} />;
+    }
+    if (!publicaciones.length) {
+      return;
+    }
+    if (!("publicaciones_key" in usuarios[key])) {
+      return;
+    }
+    const { publicaciones_key } = usuarios[key];
+    return publicaciones[publicaciones_key].map((publicacion) => (
+      <div className="pub_titulo">
+        <h2>{publicacion.title}</h2>
+        <h3>{publicacion.body}</h3>
+      </div>
+    ));
+  };
+
+  render() {
+    return (
+      <div>
+        {this.ponerUsuario()}
+        {this.ponerPublicaciones()}
+      </div>
+    );
+  }
 }
 
-const mapStateToProps = ({usuariosReducer, publicacionesReducer}) => {
-    return {
-        usuariosReducer,
-        publicacionesReducer
-    };
-}
+const mapStateToProps = ({ usuariosReducer, publicacionesReducer }) => {
+  return {
+    usuariosReducer,
+    publicacionesReducer,
+  };
+};
 
 const mapDispatchToProps = {
-    usuariosTraerTodos,
-    publicacionesTraerPorUsuario
-}
+  usuariosTraerTodos,
+  publicacionesTraerPorUsuario,
+};
 
-export default connect(mapStateToProps, mapDispatchToProps)(Publicaciones)
+export default connect(mapStateToProps, mapDispatchToProps)(Publicaciones);
